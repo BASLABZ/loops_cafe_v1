714e68e5030bc1aa91cad40f31e691df55b181c9
SuperBase <tutorialsuper.cs@gmail.com>
2017-05-31 14:49:19 +0700

    upload assetz

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php 
+		mysql_connect("localhost",'ruangpro_userkasir','3123311091') or die("Server off");
+		mysql_select_db("ruangpro_dbkasir") or die("database not found");
+		
+ ?>
---
+++ test091.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>test</title>
+
+</head>
+<body>
+
+
+<script type="text/javascript">
+$(function(){
+      $('#example2').priceFormat({
+      prefix: 'Rp ',
+      centsSeparator: ',',
+      thousandsSeparator: '.'
+       });
+})
+</script>
+ 
+<input name="nilai" type="text" class="st-forminput" id="example2" style="width:100px" />
+</body>
+</html>
